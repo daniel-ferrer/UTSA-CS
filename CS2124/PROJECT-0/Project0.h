b52a0dd5138b8d7994ba59005a880d81465f4112
daniel-ferrer <ferrdan2506@hotmail.com>
2021-02-05 14:07:19 -0600

    added PROJECT-0 files

--- CS2124/PROJECT-0/Project0.h
+++ CS2124/PROJECT-0/Project0.h
@@ -3,3 +3,16 @@
 //1.  A C-string for the name.  You can assume that no name is more than 9 characters long, so this can be a character array of size 10 (to account for the null character).
 //2.  An integer for the ID.  Each ID is a 6-digit number.
 //3.  A double for the hourly rate.
+
+#ifndef PROJECT0_H
+#define PROJECT0_H
+
+typedef struct Employee
+{
+  int id;
+  char name[10];
+  double hrlyRate;
+} Employee;
+
+
+#endif //PROJECT0_H
