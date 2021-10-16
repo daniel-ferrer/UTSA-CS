db4fb41fe2d88992df8a8cd6f77ecde86554b1f7
Daniel Ferrer <ferrdan2506@hotmail.com>
2021-10-15 21:21:04 -0500

    tweak

--- CS3424 - Sys Prog/Quiz6/main.py
+++ CS3424 - Sys Prog/Quiz6/main.py
@@ -4,15 +4,17 @@ def main():
     # take file name as arg
     fname = str(sys.argv[1])
 
-    # open file, read ctx, close file
+    # open file
     f = open(fname, "r")
 
+    # print ctx of every even line
     count = 1
     for line in f:
         if count % 2 == 0:
             print(line)
         count+=1
 
+    # close file
     f.close()
 
 if __name__ == '__main__':
