0f424906a64e39aa06f19fdb39d70c907eb9ccbf
Daniel Ferrer <ferrdan2506@hotmail.com>
2022-10-19 19:04:37 -0500

    assign3-pt1 tweaks

--- CS3733 - Operating Sys/Fall 2022/assign3/part1.c
+++ CS3733 - Operating Sys/Fall 2022/assign3/part1.c
@@ -4,32 +4,36 @@
 #include <ctype.h>
 
 int main(int argc, char *argv[]) {
-    int i, p1Tmp;
+    int i, p1Tmp = 0;
     unsigned long LA, PA;
-    char *inFile, outFile;
+    char *inFile, *outFile;
     unsigned int pNum, fNum, dNum;
     unsigned int p = 5, f = 3, d = 7;
     unsigned int PT[32] = {2, 4, 1, 7, 3, 5, 6, 0};
 
     if(argv[1] != NULL)
         inFile = argv[1];
-    else 
-        fprintf(stderr, "No input file specified\n"); exit(-1);
-    
+    else {
+        fprintf(stderr, "No input file specified\n");
+        exit(-1);
+    }
+        
     if(argv[2] != NULL)
         outFile = argv[2];
-    else 
-        fprintf(stderr, "No output file specified\n"); exit(-1);
+    else {
+        fprintf(stderr, "No output file specified\n");
+        exit(-1);
+    }
 
     for(i = 0; i < argc; i++) {
-        if(strncmp("part1-out-test", argv[1], 14) == 0)
+        if(strncmp("part1-out-test", argv[i], 14) == 0)
             p1Tmp = 1;
     }
 
     int pageCount = 0;
     FILE *fp, *writeFile;
 
-    if(fp != NULL && writeFile != NULL) {
+    if(fp == NULL && writeFile == NULL) {
         fp = fopen(inFile, "r");
         writeFile = fopen(outFile, "w");
 
@@ -53,5 +57,9 @@ int main(int argc, char *argv[]) {
         fclose(writeFile);
     }
     else
-        fprintf(stderr, "Could not open input file\n"); exit(-1);
+    {
+        fprintf(stderr, "Could not open input file\n");
+        exit(-1);
+    }
+    return 0;
 }
